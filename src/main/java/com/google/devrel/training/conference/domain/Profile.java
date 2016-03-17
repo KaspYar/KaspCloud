419f36b669cad5fba312416b8c6e98d5b7cd208f
kaspyar <kaspyar@yahoo.com>
2016-03-17 22:29:09 +0200

    [practice #6] - make assignments from practice 6

--- src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -1,58 +1,87 @@
 package com.google.devrel.training.conference.domain;
 
+import com.google.appengine.repackaged.com.google.common.collect.ImmutableList;
 import com.google.devrel.training.conference.form.ProfileForm;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 public class Profile {
-	String displayName;
-	String mainEmail;
-	TeeShirtSize teeShirtSize;
+    String displayName;
+    String mainEmail;
+    TeeShirtSize teeShirtSize;
 
-	@Id
-	String userId;
-    
+    @Id
+    String userId;
+    // List of conferences the user has registered to attend
+    private List<String> conferenceKeysToAttend = new ArrayList<>(0);
+
+    public List<String> getConferenceKeysToAttend() {
+        return ImmutableList.copyOf(conferenceKeysToAttend);
+    }
+
+    public void addToConferenceKeysToAttend(String conferenceKey) {
+        conferenceKeysToAttend.add(conferenceKey);
+    }
+
+    /**
+     * Remove the conferenceId from conferenceIdsToAttend.
+     *
+     * @param conferenceKey a websafe String representation of the Conference Key.
+     */
+    public void unregisterFromConference(String conferenceKey) {
+        if (conferenceKeysToAttend.contains(conferenceKey)) {
+            conferenceKeysToAttend.remove(conferenceKey);
+        } else {
+            throw new IllegalArgumentException("Invalid conferenceKey: " + conferenceKey);
+        }
+    }
+
+
     /**
      * Public constructor for Profile.
-     * @param userId The user id, obtained from the email
-     * @param displayName Any string user wants us to display him/her on this system.
-     * @param mainEmail User's main e-mail address.
+     *
+     * @param userId       The user id, obtained from the email
+     * @param displayName  Any string user wants us to display him/her on this system.
+     * @param mainEmail    User's main e-mail address.
      * @param teeShirtSize The User's tee shirt size
-     * 
      */
-    public Profile (String userId, String displayName, String mainEmail, TeeShirtSize teeShirtSize) {
-    	this.userId = userId;
-    	this.displayName = displayName;
-    	this.mainEmail = mainEmail;
-    	this.teeShirtSize = teeShirtSize;
+    public Profile(String userId, String displayName, String mainEmail, TeeShirtSize teeShirtSize) {
+        this.userId = userId;
+        this.displayName = displayName;
+        this.mainEmail = mainEmail;
+        this.teeShirtSize = teeShirtSize;
     }
-    
-	public String getDisplayName() {
-		return displayName;
-	}
 
-	public String getMainEmail() {
-		return mainEmail;
-	}
+    public String getDisplayName() {
+        return displayName;
+    }
 
-	public TeeShirtSize getTeeShirtSize() {
-		return teeShirtSize;
-	}
+    public String getMainEmail() {
+        return mainEmail;
+    }
 
-	public String getUserId() {
-		return userId;
-	}
+    public TeeShirtSize getTeeShirtSize() {
+        return teeShirtSize;
+    }
 
-	/**
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
      * Just making the default constructor private.
      */
-    private Profile() {}
+    private Profile() {
+    }
 
-	public void update(String displayName, TeeShirtSize teeShirtSize) {
-		this.displayName = displayName;
-		this.teeShirtSize = teeShirtSize;
-	}
+    public void update(String displayName, TeeShirtSize teeShirtSize) {
+        this.displayName = displayName;
+        this.teeShirtSize = teeShirtSize;
+    }
 }
